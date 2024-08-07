15aeb0000c70efa0c9ab48c70ef8c90732a8cc44
chen4023 <ads4023@naver.com>
2024-08-07 17:22:12 +0900

    [React] 14일차 과제 reference

---
+++ src/components/Menu.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Item from "./Item";
+import OrderModal from "./OrderModal";
+import { useMenu } from "../context/menuContext";
+
+// 전역 상태를 사용하여 코드를 수정해 주세요.
+export function Menu() {
+  // useContext에서 menu 값 불러오기
+  const { menu } = useMenu();
+
+  const [modalOn, setModalOn] = useState(false);
+  const [modalMenu, setModalMenu] = useState(null);
+  if (!menu)
+    return (
+      <div style={{ textAlign: "center", margin: "80px" }}>
+        메뉴 정보가 없어요!
+      </div>
+    );
+
+  const categorys = Object.keys(menu);
+  return (
+    <>
+      {categorys.map((category) => {
+        return (
+          <section key={category}>
+            <h2>{category}</h2>
+            <ul className="menu">
+              {menu[category].map((item) => (
+                <Item
+                  key={item.name}
+                  item={item}
+                  clickHandler={() => {
+                    setModalMenu(item);
+                    setModalOn(true);
+                  }}
+                />
+              ))}
+            </ul>
+          </section>
+        );
+      })}
+      {modalOn ? (
+        <OrderModal modalMenu={modalMenu} setModalOn={setModalOn} />
+      ) : null}
+    </>
+  );
+}
+
+export default Menu;
